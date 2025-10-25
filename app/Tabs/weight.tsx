1f4fb6c0cd27cbb07221962088e4e1ba9bc2c122
Slana <ambrozslana@gmail.com>
2025-10-25 16:19:14 +0200

    working on weight input

--- app/Tabs/weight.tsx
+++ app/Tabs/weight.tsx
@@ -1,5 +1,5 @@
+import { useState } from "react";
 import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
-import weightData from "../Data/weightData";
 const darkBlue = "rgb(30,38,68)"
 const lightblueColor = "rgb(44, 57,103)"
 const height = Dimensions.get('window').height
@@ -7,7 +7,29 @@ const width = Dimensions.get('window').width
 const gainColor = "lightgreen"
 
 
+
 export default function Index() {
+  const [weights, setWeights] = useState({
+    monW: 0,
+    tueW: 0,
+    wedW: 0,
+    thuW: 0,
+    friW: 0,
+    satW: 10,
+    sunW: 10,
+  });
+  const weightAvg = () => {
+    const weightValues = Object.values(weights).map(w => Number(w));
+    const nonzeroCount = weightValues.filter(w => w !== 0).length;
+    return nonzeroCount === 0
+      ? 0
+      : weightValues.reduce((sum, w) => sum + w, 0) / nonzeroCount;
+  }
+  const handleWeightChange = (day: any, value: string) => {
+    const cleaned = value.replace(/[^0-9.]/g, "");
+    setWeights(prev => ({ ...prev, [day]: cleaned }));
+  };
+
   return (
     <View style={styles.container}>
 
@@ -15,55 +37,68 @@ export default function Index() {
         <Text style={styles.title}>Weight</Text>
       </View>
 
-      <View style={styles.avgContainer}>
-        <View style={[styles.singleTextContainer, { paddingTop: 10 }]}>
-          <Pressable>
+      <View style={[styles.avgContainer, { height: 60 }]}>
+        <View style={[styles.singleTextContainer, { width: 150, backgroundColor: lightblueColor, justifyContent: "flex-end" }]}>
+          <Pressable style={{ alignSelf: "center", justifyContent: "center" }}>
             {({ pressed }) => (
               <Text style={[styles.subtitle, { color: !pressed ? "white" : "grey" }]}>See All</Text>)}
           </Pressable>
         </View>
 
-        <View style={styles.singleTextContainer}>
+        <View style={[styles.singleTextContainer, { alignSelf: "center", width: 200 }]}>
           <Text style={styles.subtitle}>Week Avg.</Text>
-          <View style={[styles.singleTextContainer, { height: 30, backgroundColor: lightblueColor, flexDirection: "row", justifyContent: "center" }]}>
-            <Text style={[styles.subtitle, { color: "lightgrey", fontSize: 17 }]}>{weightData.weekAvg} </Text>
+          <View style={[styles.singleTextContainer, { height: 40, backgroundColor: lightblueColor, flexDirection: "row", justifyContent: "center" }]}>
+            <Text style={[styles.subtitle, { color: "lightgrey", fontSize: 17 }]}>{weightAvg()} </Text>
             <Text style={[styles.subtitle, { fontSize: 15, color: gainColor }]}>(0.01)</Text>
           </View>
         </View>
       </View>
 
-      <View style={[styles.avgContainer, { justifyContent: "center", borderRadius: 10, backgroundColor: "white", }]}>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.13, borderTopLeftRadius: 10, borderBottomLeftRadius: 10 }]}>
-          <Text style={styles.weightFont}>{weightData.monW} kg</Text>
-          <View style={[styles.singleTextContainer, { backgroundColor: gainColor, width: width * 0.1, height: 30 }]}>
+      <View style={[styles.avgContainer, { alignItems: "flex-start", justifyContent: "center", backgroundColor: lightblueColor, }]}>
+        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.15, borderTopLeftRadius: 8, borderBottomLeftRadius: 8 }]}>
+          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.11, height: 25 }]}>
+            <Text style={styles.weightFont}>MON </Text>
+          </View>
+          <Text style={styles.weightFont}>{weights.monW || 0} kg</Text>
+        </View>
+        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
+          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
+            <Text style={styles.weightFont}>TUE </Text>
+          </View>
+          <Text style={styles.weightFont}>{weights.tueW || "0"} kg</Text>
+        </View>
+        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
+          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
+            <Text style={styles.weightFont}>WED </Text>
+          </View>
+          <Text style={styles.weightFont}>{weights.wedW || "0"} kg</Text>
+        </View>
+        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
+          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
+            <Text style={styles.weightFont}>THU </Text>
+          </View>
+          <Text style={styles.weightFont}>{weights.thuW || "0"} kg</Text>
 
+        </View>
+        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
+          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
+            <Text style={styles.weightFont}>FRI</Text>
           </View>
+          <Text style={styles.weightFont}>{weights.friW || "0"} kg</Text>
+
         </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.13 }]}>
-          <Text style={styles.weightFont}>{weightData.tueW} kg</Text>
-          <View style={[styles.singleTextContainer, { backgroundColor: gainColor, width: width * 0.1, height: 30 }]}></View>
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.13 }]}>
-          <Text style={styles.weightFont}>{weightData.wedW} kg</Text>
-          <View style={[styles.singleTextContainer, { backgroundColor: gainColor, width: width * 0.1, height: 30 }]}></View>
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.13 }]}>
-          <Text style={styles.weightFont}>{weightData.thuW} kg</Text>
-          <View style={[styles.singleTextContainer, { backgroundColor: gainColor, width: width * 0.1, height: 30 }]}></View>
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.13 }]}>
-          <Text style={styles.weightFont}>{weightData.friW} kg</Text>
-          <View style={[styles.singleTextContainer, { backgroundColor: gainColor, width: width * 0.1, height: 30 }]}></View>
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.13 }]}>
-          <Text style={styles.weightFont}>{weightData.satW} kg</Text>
-          <View style={[styles.singleTextContainer, { backgroundColor: gainColor, width: width * 0.1, height: 30 }]}></View>
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.13, borderRightWidth: 2.5, borderTopRightRadius: 10, borderBottomRightRadius: 10 }]}>
-          <Text style={styles.weightFont}>{weightData.sunW} kg</Text>
-          <View style={[styles.singleTextContainer, { backgroundColor: gainColor, width: width * 0.1, height: 30 }]}>
-            <Text></Text>
+        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
+          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
+            <Text style={styles.weightFont}>SAT </Text>
           </View>
+          <Text style={styles.weightFont}>{weights.satW || "0"} kg</Text>
+
+        </View>
+        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.15, borderRightWidth: 2.5, borderTopRightRadius: 8, borderBottomRightRadius: 8 }]}>
+          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
+            <Text style={styles.weightFont}>SUN </Text>
+          </View>
+          <Text style={styles.weightFont}>{weights.sunW || "0"} kg</Text>
         </View>
 
       </View>
@@ -88,14 +123,13 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     position: "relative",
-
   },
   titleContainer: {
     height: height * 0.12,
     backgroundColor: darkBlue,
-    flexDirection: "row",           // lay out children horizontally
-    justifyContent: "space-between", // push title left, icon right
-    alignItems: "flex-end",           // vertically center the content
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
     paddingHorizontal: 30,
     paddingBottom: 10,
     paddingTop: height * 0.03,
@@ -103,9 +137,8 @@ const styles = StyleSheet.create({
   avgContainer: {
     height: height * 0.12,
     backgroundColor: lightblueColor,
-    flexDirection: "row",           // lay out children horizontally
-    // push title left, icon right
-    alignItems: "center",           // vertically center the content
+    flexDirection: "row",
+    alignItems: "flex-end",
     paddingHorizontal: 10,
     paddingTop: height * 0.01,
   },
@@ -115,6 +148,7 @@ const styles = StyleSheet.create({
     width: 150,
     flexDirection: "column",
     alignItems: "center",
+    justifyContent: "center",
 
   },
   subtitle: {
@@ -128,13 +162,14 @@ const styles = StyleSheet.create({
     fontWeight: "500"
   },
   weightContainer: {
-    height: 55,
+    height: 60,
     backgroundColor: lightblueColor,
     width: 150,
+    paddingTop: 8,
     flexDirection: "column",
     alignItems: "center",
     borderWidth: 2.5,
-    borderColor: "grey",
+    borderColor: darkBlue,
     borderRightWidth: 0,
   },
 })
