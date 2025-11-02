ac8a42b436cbb844df2273f5e361e31089ee3558
Slana <ambrozslana@gmail.com>
2025-11-02 11:50:08 +0100

    weight calendar and popup to set weight added

--- app/Tabs/weight.tsx
+++ app/Tabs/weight.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
+import moment from 'moment';
+import React, { useRef, useState } from "react";
+import { Animated, Dimensions, Modal, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 const darkBlue = "rgb(30,38,68)"
 const lightblueColor = "rgb(44, 57,103)"
 const height = Dimensions.get('window').height
@@ -8,100 +9,100 @@ const gainColor = "lightgreen"
 
 
 
+
 export default function Index() {
-  const [weights, setWeights] = useState({
-    monW: 0,
-    tueW: 0,
-    wedW: 0,
-    thuW: 0,
-    friW: 0,
-    satW: 10,
-    sunW: 10,
+
+  const today = moment();
+  const [selected, setSelected] = useState(today.format('YYYY-MM-DD'));
+  const [weightPopup, setWeightPopupVisible] = useState(false);
+  const startOfWeek = today.clone().startOf('isoWeek');
+  const slideAnim = useRef(new Animated.Value(1000)).current;
+  const days = Array.from({ length: 7 }, (_, i) => {
+    const d = startOfWeek.clone().add(i, 'days');
+    return {
+      label: d.format('ddd'),
+      date: d.format('YYYY-MM-DD'),
+      dayNumber: d.format('D'),
+    };
   });
-  const weightAvg = () => {
-    const weightValues = Object.values(weights).map(w => Number(w));
-    const nonzeroCount = weightValues.filter(w => w !== 0).length;
-    return nonzeroCount === 0
-      ? 0
-      : weightValues.reduce((sum, w) => sum + w, 0) / nonzeroCount;
-  }
-  const handleWeightChange = (day: any, value: string) => {
-    const cleaned = value.replace(/[^0-9.]/g, "");
-    setWeights(prev => ({ ...prev, [day]: cleaned }));
+
+  const changeWeight = (date: string) => {
+    setSelected(date);
+    setWeightPopupVisible(true);
+    slideIn();
+  };
+  const slideIn = () => {
+    Animated.timing(slideAnim, {
+      toValue: 300,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => setWeightPopupVisible(true));
   };
 
+  const slideOut = () => {
+    Animated.timing(slideAnim, {
+      toValue: 1000,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => setWeightPopupVisible(false));
+  };
   return (
     <View style={styles.container}>
+      <Modal
+        visible={weightPopup}
+        transparent={true}
+        animationType="none"
+        allowSwipeDismissal={true}
 
+        onRequestClose={() => {
+          slideOut();
+          setWeightPopupVisible(!weightPopup);
+        }}>
+        <Animated.View style={[styles.popup, { backgroundColor: "white", transform: [{ translateY: slideAnim }] }]}>
+          <Text>hello</Text>
+        </Animated.View>
+        <Pressable onPress={slideOut}>
+          <Text style={{ color: 'white' }}>Close</Text>
+        </Pressable>
+      </Modal>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Weight</Text>
       </View>
 
-      <View style={[styles.avgContainer, { height: 60 }]}>
-        <View style={[styles.singleTextContainer, { width: 150, backgroundColor: lightblueColor, justifyContent: "flex-end" }]}>
-          <Pressable style={{ alignSelf: "center", justifyContent: "center" }}>
+      <View style={[styles.avgContainer, { height: 60, alignItems: "center" }]}>
+        <View style={[styles.singleTextContainer, { width: 150, backgroundColor: lightblueColor, justifyContent: "flex-end", alignItems: "flex-end" }]}>
+          <Pressable style={{ alignSelf: "flex-start", justifyContent: "center" }}>
             {({ pressed }) => (
               <Text style={[styles.subtitle, { color: !pressed ? "white" : "grey" }]}>See All</Text>)}
           </Pressable>
         </View>
 
-        <View style={[styles.singleTextContainer, { alignSelf: "center", width: 200 }]}>
-          <Text style={styles.subtitle}>Week Avg.</Text>
-          <View style={[styles.singleTextContainer, { height: 40, backgroundColor: lightblueColor, flexDirection: "row", justifyContent: "center" }]}>
-            <Text style={[styles.subtitle, { color: "lightgrey", fontSize: 17 }]}>{weightAvg()} </Text>
-            <Text style={[styles.subtitle, { fontSize: 15, color: gainColor }]}>(0.01)</Text>
+        <View style={[styles.singleTextContainer, { backgroundColor: lightblueColor, paddingRight: 15, flexDirection: "row", width: 200, justifyContent: "space-between", alignItems: "flex-end" }]}>
+          <View >
+            <Text style={styles.subtitle}>Week Avg</Text>
           </View>
+
+          <Text style={[styles.subtitle, { color: "lightgrey", fontSize: 17 }]}>{20} </Text>
+          <Text style={[styles.subtitle, { fontSize: 15, color: gainColor }]}>(0.01)</Text>
         </View>
       </View>
-
-      <View style={[styles.avgContainer, { alignItems: "flex-start", justifyContent: "center", backgroundColor: lightblueColor, }]}>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.15, borderTopLeftRadius: 8, borderBottomLeftRadius: 8 }]}>
-          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.11, height: 25 }]}>
-            <Text style={styles.weightFont}>MON </Text>
-          </View>
-          <Text style={styles.weightFont}>{weights.monW || 0} kg</Text>
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
-          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
-            <Text style={styles.weightFont}>TUE </Text>
-          </View>
-          <Text style={styles.weightFont}>{weights.tueW || "0"} kg</Text>
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
-          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
-            <Text style={styles.weightFont}>WED </Text>
-          </View>
-          <Text style={styles.weightFont}>{weights.wedW || "0"} kg</Text>
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
-          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
-            <Text style={styles.weightFont}>THU </Text>
-          </View>
-          <Text style={styles.weightFont}>{weights.thuW || "0"} kg</Text>
-
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
-          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
-            <Text style={styles.weightFont}>FRI</Text>
-          </View>
-          <Text style={styles.weightFont}>{weights.friW || "0"} kg</Text>
-
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.14 }]}>
-          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
-            <Text style={styles.weightFont}>SAT </Text>
-          </View>
-          <Text style={styles.weightFont}>{weights.satW || "0"} kg</Text>
-
-        </View>
-        <View style={[styles.weightContainer, { backgroundColor: "white", width: width * 0.15, borderRightWidth: 2.5, borderTopRightRadius: 8, borderBottomRightRadius: 8 }]}>
-          <View style={[styles.singleTextContainer, { borderRadius: 7, backgroundColor: gainColor, width: width * 0.1, height: 25 }]}>
-            <Text style={styles.weightFont}>SUN </Text>
-          </View>
-          <Text style={styles.weightFont}>{weights.sunW || "0"} kg</Text>
-        </View>
-
+      <View style={styles.weekRow}>
+        {days.map((d) => (
+          <TouchableOpacity
+            key={d.date}
+            style={[
+              styles.day,
+              d.date === selected && styles.selectedDay,
+              d.date === today.format('YYYY-MM-DD') && styles.todayDay,
+            ]}
+            onPress={() => changeWeight(d.date)}
+          >
+            <Text style={styles.weightFont}>{d.label}</Text>
+            <Text style={styles.number}>{d.dayNumber}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
+
       <View style={styles.titleContainer}>
         <Text style={styles.title}>select menu</Text>
       </View>
@@ -118,11 +119,52 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: lightblueColor,
   },
+  weekRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 10,
+    backgroundColor: lightblueColor,
+  },
+  day: {
+    alignItems: 'center',
+    width: 45,
+    height: 80,
+    borderRadius: 8,
+    paddingTop: 5,
+    backgroundColor: "white",
+
+  },
+  selectedDay: {
+    backgroundColor: 'white',
+  },
+  todayDay: {
+    backgroundColor: gainColor,
+  },
+
+  number: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
+  },
   title: {
     fontSize: 32,
     color: "white",
     fontWeight: "bold",
     position: "relative",
+  },
+  popup: {
+    height: height * 0.12,
+    width: 200,
+    alignSelf: "center",
+    backgroundColor: lightblueColor,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingTop: height * 0.01,
+    borderRadius: 10,
+
+
+
   },
   titleContainer: {
     height: height * 0.12,
@@ -157,7 +199,7 @@ const styles = StyleSheet.create({
     color: "white"
   },
   weightFont: {
-    fontSize: 12,
+    fontSize: 13,
     color: "grey",
     fontWeight: "500"
   },
@@ -172,4 +214,6 @@ const styles = StyleSheet.create({
     borderColor: darkBlue,
     borderRightWidth: 0,
   },
+
+
 })
